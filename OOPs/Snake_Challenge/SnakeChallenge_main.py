0012a2da0737739c5f0dd2465dc60753a83b65ed
Chitra0106 <89143969+Chitra0106@users.noreply.github.com>
2026-08-20 13:00:11 +0530

    Add Python code

--- OOPs/Snake_Challenge/SnakeChallenge_main.py
+++ OOPs/Snake_Challenge/SnakeChallenge_main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 import time
 from Snake import Snake
 from Food import Food
+from ScoreBoard import ScoreBoard
 
 screen= Screen()
 screen.setup(width=600, height=600)
@@ -11,22 +12,38 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
-screen.onkey(snake.left, "Right")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.2)
     snake.move()
+
     #Detect the food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    #Detect collision with wall
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        scoreboard.game_over()
+        game_is_on = False
+
+    #Detect Collision with Tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
 
 screen.exitonclick()
 
